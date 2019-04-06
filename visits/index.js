da4279152d95a68f4b21b47c5b2b7d168a0ea73d
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-04-06 15:55:30 -0400

    Docker Compose

---
+++ visits/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+const client = redis.createClient({
+    // Picked from docker compose 
+    host: 'redis-server',
+    port: 6379
+});
+client.set('visits',0);
+
+app.get('/', (req,res) => {
+    client.get('visits', (err,visits) => {
+        res.send(`Number of visits is ${visits}`)
+        client.set('visits',parseInt(visits) + 1)
+    });
+});
+
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log(`Listerninig on port ${PORT}` )
+})
