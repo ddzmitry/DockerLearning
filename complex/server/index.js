a34d7d1003f51822eae5db0504f924e5566b9d98
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-04-15 23:08:35 -0400

    server

--- complex/server/index.js
+++ complex/server/index.js
@@ -1,10 +1,8 @@
 const keys = require("./keys");
-
 // Express APP setup
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -35,3 +33,37 @@ const redisClient = redis.createClient({
 
 const redisPublisher = redisClient.duplicate();
 
+app.get('/',(req,res) => {
+    res.send("hi");
+});
+
+app.get('/values/all', async (req,res) => {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req,res) => {
+    redisClient.hgetall('values',(err,values)=>{
+        res.send(values);
+    });
+});
+
+app.post('/values', async(req,res) => {
+    const index = req.body.index;
+    if(parseInt(index) > 40){
+        return res.status(422).send('Index too high');
+
+    }
+    // No value assigned yet
+    redisClient.hset('values',index,'nothing yet')
+    redisPublisher.publish('insert',index);
+    // Submit index to PQ
+    pgClient.query('INSERT INTO values(number) values($1)',[index]);
+    // 
+    res.send({working : true});
+
+})
+
+app.listen(5000,err => {
+    console.log("Listerning")
+})
